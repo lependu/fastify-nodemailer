eb24c5b8f1cc8d3c7044206d5679dbab21dea3dc
gendronb <bruno.gendron.consult@gmail.com>
2019-03-19 14:25:45 -0400

    Updated readme and test

--- test.js
+++ test.js
@@ -63,3 +63,40 @@ test('nodemailer#sendMail', t => {
       })
     })
 })
+
+test('customTransport', t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+  t.tearDown(fastify.close.bind(fastify))
+
+  let transport = {
+    name: 'minimal',
+    version: '0.1.0',
+    send: (mail, callback) => {
+      let envelope = mail.message.getEnvelope()
+      let messageId = mail.message.messageId()
+      callback(null, {
+        envelope,
+        messageId
+      })
+    }
+  }
+
+  fastify
+    .register(nodemailer, transport)
+    .ready(err => {
+      t.error(err)
+      let { nodemailer } = fastify
+      nodemailer.sendMail({
+        from: 'sender@example.com',
+        to: 'recipient@example.com',
+        subject: 'foo',
+        text: 'bar'
+      }, (err, info) => {
+        t.error(err)
+        t.equal(info.envelope.from, 'sender@example.com')
+        t.equal(info.envelope.to[0], 'recipient@example.com')
+      })
+    })
+})
